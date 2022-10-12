341a732de6dd69386449d4c312fb55f6a93fb77c
jopiedra <68862066+Jopiedra@users.noreply.github.com>
2022-10-12 10:13:30 -0600

    Initial Commit

---
+++ lambda_function.py
@@ -0,0 +1,27 @@
+import json
+
+def lambda_handler(event, context):
+    return {
+        'statusCode': 200,
+        'body': json.dumps('Hello from Lambda!')
+    }
+
+context = {
+    "aws_request_id" : "12345ABC"
+}
+
+event = {
+    "version": "0",
+    "id": "56585b50-91d2-c781-41a0-def74a4b5e07",
+    "detail-type": "Scheduled Event",
+    "source": "aws.events",
+    "account": "964679837161",
+    "time": "2022-09-20T20:11:37Z",
+    "region": "us-east-2",
+    "resources": [
+        "arn:aws:events:us-east-2:964679837161:rule/Lendio_CheckStatus_Dev"
+    ],
+    "detail": {}
+}
+
+print(lambda_handler(event, context))
