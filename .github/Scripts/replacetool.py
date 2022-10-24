8aec2ec816087b15d99268c96c230c704a018f66
Jopiedra <68862066+Jopiedra@users.noreply.github.com>
2022-10-24 10:49:17 -0600

    Update replacetool.py

--- .github/Scripts/replacetool.py
+++ .github/Scripts/replacetool.py
@@ -4,9 +4,13 @@ filetosearch = sys.argv[1]
 texttoreplace = sys.argv[2]
 texttoinsert = sys.argv[3]
 
-s = open(filetosearch).read()
-s = s.replace(texttoreplace, texttoinsert)
-f = open(filetosearch, 'w')
-f.write(s)
-f.close()
-quit()
+print(filetosearch)
+print(texttoreplace)
+print(texttoinsert)
+
+# s = open(filetosearch).read()
+# s = s.replace(texttoreplace, texttoinsert)
+# f = open(filetosearch, 'w')
+# f.write(s)
+# f.close()
+# quit()
