25c5e572080c05c59d9eb5814a72d3ccdb98e84c
Jopiedra <68862066+Jopiedra@users.noreply.github.com>
2022-10-25 10:11:37 -0600

    Update replacetool.py

--- .github/Scripts/replacetool.py
+++ .github/Scripts/replacetool.py
@@ -1,16 +1,52 @@
-import sys
+import sys, os
+from xml.dom import ValidationErr
 
-filetosearch = sys.argv[1]
-texttoreplace = sys.argv[2]
-texttoinsert = sys.argv[3]
+def mngerror():
+    print("==================================================================")
+    print("REPLACETOOL V1")
+    print("==================================================================")
+    print("")
+    print("This tool replace a string value by other inside a document")
+    print("")
+    print("Call example:")
+    print(" >>python REPLACETOOL.py FilePath SearchText ReplaceText")
+    print("")
+    print("     FilePath:       File path of the file to check.")
+    print("     SearchText:     Text to search for inside the file.")
+    print("     ReplaceText:    Text to be replaced with.")
+    print("")
+    print("==================================================================")
+    raise SystemExit("")
 
-# print(filetosearch)
-# print(texttoreplace)
-# print(texttoinsert)
+def main(filepath, searchtxt, replacetxt):
+    s = open(filepath).read()
+    s = s.replace(searchtxt, replacetxt)
+    f = open(filepath, 'w')
+    f.write(s)
+    f.close()
+    print("**** REPLACE SUCCESS. ****")
+    print("")
+    quit()
 
-s = open(filetosearch).read()
-s = s.replace(texttoreplace, texttoinsert)
-f = open(filetosearch, 'w')
-f.write(s)
-f.close()
-quit()
+if __name__ == "__main__":
+    try:
+        filePath = sys.argv[1]
+    except IndexError:
+        print("**** REPLACETOOL ERROR: Missing Path file parameter. ****")
+        mngerror()
+    try:
+        searchTxt = sys.argv[2]
+    except IndexError:
+        print("**** REPLACETOOL ERROR: Missing Search Text parameter. ****")
+        mngerror()
+    try:
+        replaceTxt = sys.argv[3]
+    except IndexError:
+        print("**** REPLACETOOL ERROR: Missing Replace text parameter. ****")
+        mngerror()
+
+    fileExist = os.path.exists(filePath)
+    if fileExist:
+        main(filePath, searchTxt, replaceTxt)
+    else:
+        print("**** REPLACETOOL ERROR: File was not found or not exists. ****")
